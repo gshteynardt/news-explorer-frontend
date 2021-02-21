4b45ea00f772d05657925e9309571f0d7190fe5e
GermanShtey <gvshteynardt@gmail.com>
2021-02-21 23:27:30 +0300

    изменить URL сервера

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 export const ApiKey = `8fa29a8750fd4c7a82906a72eb94a49b`;
 export const NewsApiUrl = `https://newsapi.org/v2/`;
 export const BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://api.shteyn.students.nomoredomains.monster'
+  ? 'https://api.shtey.students.nomoredomains.monster'
   : 'http://localhost:3000';
 
 export default {
